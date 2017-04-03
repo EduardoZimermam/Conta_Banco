1fdc34a98d7f652c181761a198195a8cd889b308
Eduardo Zimermam <eduardozimerman@live.com>
2017-04-03 14:08:26 -0300

    Criação das primeiras classes e métodos. Formulação geral do programa.

---
+++ Banco.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Conta{
+	double saldo;
+	String num_conta;
+
+	void deposita(double valor){
+	}
+
+	void retira(double valor){
+	}
+
+	double getsaldo(){
+	}
+}
+
+class Corrente extends Conta{
+	
+}
